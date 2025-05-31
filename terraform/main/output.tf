f3e4f7e29f71d851165d5675d9f3b84224d17c66
Adham Basheir <adhambasheir000@gmail.com>
2025-05-31 18:27:13 +0300

    trail 5.2

--- terraform/main/output.tf
+++ terraform/main/output.tf
@@ -1,5 +1,4 @@
 output "kubeconfig" {
-  value = "gcloud container clusters get-credentials ${module.gke.cluster_name} --region ${var.region} --project ${var.project_id}"
-  # value = "gcloud container clusters get-credentials ${module.gke.cluster_name} --region ${module.gke.location} --project ${var.project_id}"
+  value = "gcloud container clusters get-credentials ${module.gke.cluster_name} --region ${module.gke.location} --project ${var.project_id}"
   description = "Command to get kubeconfig for the GKE cluster"
 }
