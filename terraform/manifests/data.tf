a4672318524fb681d17997112bf1bbdc5897ab11
Adham Basheir <adhambasheir000@gmail.com>
2025-06-09 22:12:16 +0300

    trail 7.4

--- terraform/manifests/data.tf
+++ terraform/manifests/data.tf
@@ -25,7 +25,3 @@ data "terraform_remote_state" "platform" {
 data "local_file" "app_of_apps" {
   filename = "${path.module}/argocd.yaml"
 }
-
-# data "local_file" "cert" {
-#   filename = "${path.module}/cert.yaml"
-# }
