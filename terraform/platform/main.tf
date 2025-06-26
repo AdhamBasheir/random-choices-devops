d4cf4948ce0fc1ce322b1155120063e0e64bd699
Adham Basheir <adhambasheir000@gmail.com>
2025-06-27 00:53:25 +0300

    CI: remove Jenkins CI configuration

--- terraform/platform/main.tf
+++ terraform/platform/main.tf
@@ -14,12 +14,6 @@ module "argocd" {
   cluster_ca_certificate = local.cluster_ca_certificate
 }
 
-# module "jenkins" {
-#   source                 = "./modules/jenkins"
-#   cluster_endpoint       = local.cluster_endpoint
-#   cluster_ca_certificate = local.cluster_ca_certificate
-# }
-
 module "monitoring" {
   source                 = "./modules/monitoring"
   cluster_endpoint       = local.cluster_endpoint
